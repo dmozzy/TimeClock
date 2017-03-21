d64225e380136642696df19a59987d81c278202d
Analytic <albrecht.allenstein@gmail.com>
2017-03-21 22:26:47 +0100

    Compatibility with Tuffle 3

--- migrations/2_TruffleTestMigration.js
+++ migrations/2_TruffleTestMigration.js
@@ -1,3 +1,5 @@
+var TimeClock = artifacts.require("./TimeClock.sol");
+
 module.exports = function(deployer) {
     deployer.deploy(TimeClock, "This is the unit test contract",1, 1, 10, web3.toWei(0.01, 'ether'));
 };
--- truffle.js
+++ truffle.js
@@ -1,5 +1,7 @@
+var DefaultBuilder = require("truffle-default-builder");
+
 module.exports = {
-  build: {
+  build: new DefaultBuilder({
     "index.html": "index.html",
     "app.js": [
       "javascripts/mustache.min.js",
@@ -9,9 +11,26 @@ module.exports = {
       "stylesheets/pure-min.css",
       "stylesheets/app.css"
     ]
-  },
+  }),
   rpc: {
     host: "localhost",
     port: 8545
+  },
+  networks: {
+    dev: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*"
+    },
+    staging: {
+      host: "localhost",
+      port: 8546,
+      network_id: 1337
+    },
+    ropsten: {
+      host: "158.253.8.12",
+      port: 8545,
+      network_id: 3
+    }
   }
 };
